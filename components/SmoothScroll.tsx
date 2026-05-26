ee7d7e91ca2003096459f5159b7cb1b00f04def4
priyex <jayspriye@gmail.com>
2026-05-26 01:46:34 +0100

    feat: set up smooth scroll and update theme styles
    - simplify lenis initialization in SmoothScroll component
    - add required lenis css styles to fix scroll container behavior
    - update root background and foreground color variables

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,16 +6,7 @@ import Lenis from '@studio-freight/lenis';
 
 export default function SmoothScroll() {
     useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            direction: 'vertical',
-            gestureDirection: 'vertical',
-            smooth: true,
-            mouseMultiplier: 1,
-            smoothTouch: false,
-            touchMultiplier: 2,
-        });
+        const lenis = new Lenis();
 
         function raf(time: number) {
             lenis.raf(time);
